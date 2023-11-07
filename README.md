01af7d78ca207d99c7f5fbe56b970e81289ec69f
Micado Pierre <Biml16gb@gmail.com>
2023-11-07 09:15:35 -0500

    backend-blogapp

---
+++ README.md
@@ -0,0 +1,13 @@
+## Backend Repo
+
+This is the backend for a full stack blog app. It is built using Express and MongoDB.
+
+## Build Instructions
+1. git clone https://github.com/Cado001/frontend-blogapp
+cd frontend-blogapp
+3. obtain .env for environmental variables
+4. npm i 
+5. node .
+
+## Frontend Repo
+[Click here to view the repo for the frontend](https://github.com/Cado001/frontend-blogapp)
--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-import express from 'express';
-import cors from 'cors';
-import { MongoClient } from 'mongodb';
+import express from 'express'
+import cors from 'cors'
+import { MongoClient } from 'mongodb'
 
+import 'dotenv/config'
 const app = express()
 app.use(cors())
-
-
-
-
-const MONGO_URI= "mongodb+srv://biml16gb:mEK8ggrwk1uBRB2J@myfirstcluster.fctdvbv.mongodb.net/?retryWrites=true&w=majority"
-const client = new MongoClient(MONGO_URI)
+app.use(express.json())
+const client = new MongoClient(process.env.MONGO_URI)
 const db = client.db('blogapp-c12')
-const blog = db.collection('blog-post')
-
+const blogPosts = db.collection('blog-posts')
+const userDb = db.collection('users')
 client.connect()
 console.log('Connected to Mongo')
-
-
-app.get('/', async (req,res) => {
-const allPosts = await blogPosts.find().toArray()
-console.log('allPost -> ', allPosts)
-    res.send('here are some blog post, not yet! 🐶')
-} )
-
+app.get('/', async (req, res) => {
+    const allPosts = await blogPosts.find().toArray()
+    console.log('allPosts -> ', allPosts)
+    res.send(allPosts)
+})
+app.post('/', async (req, res) => {
+    const newBlogPost = { title: req.body.title, content: req.body.content }
+    await blogPosts.insertOne(newBlogPost)
+    const allPosts = await blogPosts.find().toArray()
+    res.send(allPosts)
+})
+// sign up
+app.post('/signup', async (req, res) => {
+    const userAdded = await userDb.insertOne({ email: req.body.email, password: req.body.password })
+    console.log('user added ->', userAdded)
+    res.send(userAdded)
+})
+// log in
+app.post('/login', (req, res) => {
+    console.log(req.body)
+})
 app.listen('8080', () => console.log('Api listening on port 8080 😎'))
